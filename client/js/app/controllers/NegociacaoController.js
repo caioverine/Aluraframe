dd36437229c3026ce284fad85fee623a4889b364
caio.cvac <caio.cvac@dpf.gov.br>
2018-02-21 17:03:55 -0300

    Parte 2 Aula 03

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,40 +3,18 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
-        let self = this;
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
 
-            get(target, prop, receiver) {
-
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-
-                    return function(){
-
-                    console.log(`método '${prop}' interceptado`);
-
-                    Reflect.apply(target[prop], target, arguments);
-
-                    self._negociacoesView.update(target);
-
-                    }
-                }
-
-                return Reflect.get(target, prop, receiver);
-            }
-        });
-        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
         
-        this._mensagem = new Mensagem();
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), this._negociacoesView, 'adiciona', 'esvazia')
+        
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
         
+        this._mensagem = new Bind(new Mensagem(), this._mensagemView, 'texto')
     }
     
     adiciona(event) {
@@ -46,7 +24,6 @@ class NegociacaoController {
         this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         
         this._limpaFormulario();   
     }
